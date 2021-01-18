e2d43e92ca1ae4bc797f78326d89196633ca8cf6
LizzyKate <onyenekwelizabeth@gmail.com>
2021-01-18 20:08:45 +0100

    building blog

---
+++ blog/index.js
@@ -0,0 +1,17 @@
+// Vue.component("greeting", {
+//   template: "<div><p>I am a {{title}} tag</p>",
+//   data() {
+//     return {
+//       title: "lizzy",
+//     };
+//   },
+// });
+import data from "blog.js";
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      title: "lizzy is bautiful",
+    };
+  },
+});
